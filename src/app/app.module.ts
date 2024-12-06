1c1b3b83dbef9613fc2cae1174acc6e9af21d978
eugenekotov <ek150177@gmail.com>
2024-12-07 00:27:33 +0200

    inital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './components/container/container.component';
+import { ColorComponent } from './components/color/color.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    ColorComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
