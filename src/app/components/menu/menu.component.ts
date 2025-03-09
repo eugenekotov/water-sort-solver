e783db479904416742aaa585a1946f1059998736
eugenekotov <ek150177@gmail.com>
2025-03-09 23:29:02 +0200

    main menu disabled

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -4,6 +4,7 @@ import { MainService, TView } from 'src/app/services/main.service';
 class MenuItem {
   title_param: string;
   view: TView;
+  disabled: boolean | (() => boolean);
 }
 
 @Component({
@@ -16,18 +17,22 @@ export class MenuComponent {
   protected readonly view: TView = 'menu';
 
   menu: MenuItem[] = [
-    { title_param: "MENU.PLAY", view: "play" },
-    { title_param: "MENU.CREATE", view: "setup" },
-    { title_param: "MENU.SOLVE", view: "solve" },
-    { title_param: "MENU.SAVE", view: "save" },
-    { title_param: "MENU.LOAD", view: "load" },
-    { title_param: "MENU.SETTINGS", view: "settings" }
+    { title_param: "MENU.PLAY", view: "play", disabled: false },
+    { title_param: "MENU.CREATE", view: "setup", disabled: false },
+    { title_param: "MENU.SOLVE", view: "solve", disabled: false },
+    { title_param: "MENU.SAVE", view: "save", disabled: () => this.mainService.game !== undefined },
+    { title_param: "MENU.LOAD", view: "load", disabled: false },
+    { title_param: "MENU.SETTINGS", view: "settings", disabled: false }
   ];
 
   constructor(public mainService: MainService) { }
 
-  onClick(item: MenuItem) {
+  protected onClick(item: MenuItem) {
     this.mainService.setView(item.view);
+  }
+
+  protected itemDisabled(item: MenuItem) {
+    return typeof item.disabled === 'function' ? item.disabled() : item.disabled;
   }
 
 }
